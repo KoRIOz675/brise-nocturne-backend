411e0f14ad1587f50aa99b7ce858521109ee25a3
KoRIOz675 <thomas@mafille.net>
2025-04-03 19:17:07 +0200

    MAJOR: build: Initial commit

---
+++ src/main/java/net/brise_nocturne/brise_nocturne_backend/BriseNocturneBackendApplication.java
@@ -0,0 +1,13 @@
+package net.brise_nocturne.brise_nocturne_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriseNocturneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BriseNocturneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/brise_nocturne/brise_nocturne_backend/BriseNocturneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.brise_nocturne.brise_nocturne_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriseNocturneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
